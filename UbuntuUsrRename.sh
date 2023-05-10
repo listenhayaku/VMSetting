9c53249a53a81c774a1584c77a4c917953a004f2
listenhayaku <hhc8811105@ccit.ndu.edu.tw>
2023-05-10 21:34:20 +0800

    OK

--- UbuntuUsrRename.sh
+++ UbuntuUsrRename.sh
@@ -3,6 +3,12 @@
 target1="/etc/passwd"
 target2="/etc/shadow"
 target3="/etc/group"
+whoami=$(whoami)
+
+if [ $whoami != "root" ]; then
+	echo "you are not root"
+	exit 0
+fi
 
 if [[ $1 == "" ]] || [[ $2 == "" ]]; then
 	echo "UbuntuUsrRename.sh usage: ./UbuntuUsrRename.sh {oldusername} {newusername}"
@@ -13,3 +19,6 @@ sed -i "s/$1/$2/g" $target1
 sed -i "s/$1/$2/g" $target2
 sed -i "s/$1/$2/g" $target3
 
+mv /home/$1 /home/$2
+
+passwd $2
